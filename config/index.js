3bf6f8bbcfc6bd91ec3c235c4e8b47e84b926b98
Tristan Lovell <tristan@fuzeus.com>
2015-06-25 10:37:07 -0700

    Split config file into dev and prod

--- config/index.js
+++ config/index.js
@@ -1,6 +1,12 @@
-var nconf = require('nconf');
-var path = require('path');
+var nconf = require('nconf')
+  , path = require('path')
+  , logger = require('nlogger').logger(module);
 
-nconf.file(path.join(__dirname, 'config.json'));
-
+if (process.env.NODE_ENV === 'prod') {
+  logger.info('loaded config-prod.json');
+  nconf.file(path.join(__dirname, 'config-prod.json'));
+} else {
+  logger.info('loaded config-dev.json');
+  nconf.file(path.join(__dirname, 'config-dev.json'));
+}
 module.exports = nconf;
