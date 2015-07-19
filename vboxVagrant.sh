cb65d7fd9ead0276afd680b62497a4647d689fb4
Jere Krischel <jere@krischel.org>
2015-07-19 16:08:04 -0700

    jere: adding osx vbox/vagrant install script

---
+++ vboxVagrant.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd "$( dirname "${BASH_SOURCE[0]}" )";
+
+ps -ef|grep VBox|grep -v grep|awk '{print $2}'|xargs -I {} sudo kill -9 {}
+
+# install latest virtualbox
+vboxLatest=$(curl -s http://download.virtualbox.org/virtualbox/LATEST.TXT)
+wget http://download.virtualbox.org/virtualbox/$vboxLatest/VBoxGuestAdditions_$vboxLatest.iso \
+     -O ~/Downloads/VBoxGuestAdditions_$vboxLatest.iso
+
+curl -s http://download.virtualbox.org/virtualbox/$vboxLatest/|grep dmg \
+     |perl -lpe 's/(.*)(HREF=")(VirtualBox-'$vboxLatest'-\d+-OSX.dmg)(".*)/$3/g' \
+     | xargs -I {} wget http://download.virtualbox.org/virtualbox/$vboxLatest/{} -O ~/Downloads/vbox.dmg
+hdiutil attach ~/Downloads/vbox.dmg
+sudo installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /
+hdiutil detach /Volumes/VirtualBox/
+
+# install latest vagrant
+curl -s https://www.vagrantup.com/downloads.html|grep dmg \
+     |perl -lpe 's/(.*)(href=")(.*.dmg)(.*)/$3/g'| xargs -I {} wget {} -O ~/Downloads/vagrant.dmg
+hdiutil attach ~/Downloads/vagrant.dmg
+sudo installer -pkg /Volumes/Vagrant/Vagrant.pkg -target /
+hdiutil detach /Volumes/Vagrant/
