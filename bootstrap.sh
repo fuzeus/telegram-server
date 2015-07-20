0e69beb273d93dc3e6ba1f1ca1cb1f917470f6fc
Jere Krischel <jere@krischel.org>
2015-07-19 17:31:17 -0700

    jere: updated packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,12 @@ function runTimedCommand
     echo "$(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed"
 }
 
-runTimedCommand 'sudo apt-get update -y'
-runTimedCommand 'sudo apt-get upgrade -y'
-runTimedCommand 'sudo apt-get dist-upgrade -y'
-runTimedCommand 'sudo apt-get install nginx -y'
+# optional if you want to run the latest and greatest packages
+# runTimedCommand 'sudo apt-get update -y'
+# runTimedCommand 'sudo apt-get upgrade -y'
+# runTimedCommand 'sudo apt-get dist-upgrade -y'
+
+runTimedCommand 'sudo apt-get install nginx nodejs npm mongodb -y'
+runTimedCommand 'sudo npm install -g ember-cli'
+runTimedCommand 'sudo ln -s /usr/bin/nodejs /usr/bin/node' 
+runTimedCommand 'cd /home/vagrant/telegram-server; sudo -u vagrant npm install' 
